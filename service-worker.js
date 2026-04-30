62c2da7177fee38eb2b0a9cbeefc2c2937044f02
juanoil <info@juanalghrbia.com>
2026-04-30 20:19:57 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'vouchers-cache-v1';
+const CACHE_NAME = 'vouchers-cache-v2';
+
+// القائمة بالملفات التي سيتم حفظها
 const urlsToCache = [
   './',
   './index.html',
@@ -7,13 +9,31 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // تفعيل التحديث فوراً
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// مسح النسخ القديمة من ذاكرة الجوال
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// الأولوية لجلب التعديلات الجديدة من الإنترنت، وإذا لم يوجد إنترنت يفتح من الذاكرة
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
